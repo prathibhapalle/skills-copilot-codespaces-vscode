81989069f79bd61e3e48c99b4c93b7e35e6bf201
prathibhapalle <131138167+prathibhapalle@users.noreply.github.com>
2023-12-15 04:53:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,22 @@
+// Create web server application that can respond to requests for comments
+// from the comments.json file.
+// 1. Load the comments.json file
+// 2. Create a web server
+// 3. Respond to requests for comments
+
+const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
+    fs.readFile('./comments.json', (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.end(data);
+    });
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+server.listen(3000, () => {
+    console.log('Server listening on port 3000');
 });
